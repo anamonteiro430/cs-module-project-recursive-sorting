7ddbd4208fa04a178b058b0c34e684cf170b3595
kika <anamonteiro430@gmail.com>
2020-05-20 00:56:28 +0100

    merge sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,18 +1,47 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
+# Helper Function
+# Merges 2 sorted piecs back together into a single sorted list
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    merged_arr = [0] * elements # initializing an array
 
-    # Your code here
+    l = r = 0 #index of left an right array
 
+    for i in range(elements): # i is index of new array, [0,0,...]
+        if l >= len(arrA): 
+            merged_arr[i] = arrB[r]
+            r += 1
+        elif r >= len(arrB):
+            merged_arr[i] = arrA[l]
+            l += 1
+        
+        if arrA[l] < arrB[r]:
+            merged_arr[i] = arrA[l]
+            l += 1
+        else:
+            merged_arr[i] = arrB[r]
+            r += 1
+
+    #starting at the beggining on arrA and arrB
+
+    
 
     return merged_arr
 
 
-# TO-DO: implement the Merge Sort function below USING RECURSION
+# Divides the original list into sublists
+# Until there's only 1 item in the list
 def merge_sort(arr):
-    # Your code here
-
+    # Base Case
+    if len(arr) > 1: # keep splitting in half
+       
+        #find middle index
+        middle = arr[0] + len(arr) // 2
+        # call merge_sort on Left
+        left = merge_sort(arr[0:middle])
+        # call merge_sort on Right
+        right = merge_sort(arr[middle+1:])
+        # merge sorted sides
+        arr = merge(left, right)
 
     return arr
 
@@ -38,3 +67,5 @@ def timsort(arr):
     # Your code here
 
     return arr
+
+
